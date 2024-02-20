3f244bbcd57a11d37fb9eb3d511004d6a41b3690
Daddyjohn63 <john@webandprosper.co.uk>
2024-02-20 13:37:39 +0000

    added zustand and fixed mobile navbar, so closes on click of link

--- app/(dashboard)/_components/mobile-sidebar.jsx
+++ app/(dashboard)/_components/mobile-sidebar.jsx
@@ -1,7 +1,12 @@
+'use client';
 import { Menu } from 'lucide-react';
 
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Sidebar } from './sidebar';
+import { useMobileSidebar } from '@/hooks/use-mobile-sidebar';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 /**
  * component to render mobile sidebar
@@ -12,14 +17,36 @@ import { Sidebar } from './sidebar';
  * @returns
  */
 export const MobileSidebar = () => {
+  const pathname = usePathname();
+
+  const [isMounted, setIsMounted] = useState(false);
+
+  const onOpen = useMobileSidebar(state => state.onOpen);
+  const onClose = useMobileSidebar(state => state.onClose);
+  const isOpen = useMobileSidebar(state => state.isOpen);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    onClose();
+  }, [pathname, onClose]);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
-    <Sheet>
-      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
-        <Menu />
-      </SheetTrigger>
-      <SheetContent side="left" className="p-0 bg-white">
-        <Sidebar />
-      </SheetContent>
-    </Sheet>
+    <>
+      <Button onClick={onOpen} className="block md:hidden" variant="ghost" size="sm">
+        <Menu className="h-6 w-6" />
+      </Button>
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent side="left" className="p-2 pt-10">
+          <Sidebar />
+        </SheetContent>
+      </Sheet>
+    </>
   );
 };
